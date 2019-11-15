e7c61558cc02bb135fb09c5e8c8ab6016c7aceb8
admin <admin@admins-MacBook-Pro.local>
2019-11-15 08:34:46 -0800

    register and login and token completed

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const validateUser = require('../users/users-helper.js')
 const Users = require('../users/users-model.js')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -9,6 +11,9 @@ router.post('/register', (req, res) => {
   const validatedUser = validateUser(newUser)
 
   if(validatedUser.isSuccessful === true){
+    const hash = bcrypt.hashSync(newUser.password, 12)
+    newUser.password = hash
+
     Users.add(newUser)
     .then(user => {
       return res.status(201).json(user)
@@ -25,9 +30,38 @@ router.post('/login', (req, res) => {
   // implement login
   let {username, password} = req.body
 
+  if(username && password){
+    Users.findByUser(username)
+    .then(account => {
+      if(account && bcrypt.compareSync(password, account.password)){
+          const token = getJwtToken(account.username)
 
-
-
+          res.status(201).json({
+            message: `Welcome ${account.username}`,
+            token
+          })
+      } else {
+        res.status(500).json({message: "Please provide valid username and password"})
+      }
+    })
+    .catch(error => {
+      res.status(401).json({message: "Username was not found"})
+    })
+  } else {
+      res.status(401).json({message: "Please provide valid username and password"})
+  }
 });
+
+
+function getJwtToken(username){
+  const payload = {
+    username
+  }
+  const secret = process.env.JWT_SECRET || 'willy wonka'
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
